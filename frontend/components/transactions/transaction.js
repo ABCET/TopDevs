c178336592d5f749c1465553887df7dd5abcee5e
MVK2803 <mathewvinukariath@gmail.com>
2023-05-04 17:45:34 +0530

    .

--- frontend/components/transactions/transaction.js
+++ frontend/components/transactions/transaction.js
@@ -1,8 +1,45 @@
+let Data=[
+  {
+    "ID": 1,
+    "Amount": 100,
+    "Brand": "MAthew",
+    "Category": "Lifestyle",
+    "Date": "11-12-2023"
+  },
+  {
+    "ID": 2,
+    "Amount": 200,
+    "Brand": "Acme",
+    "Category": "Beauty",
+    "Date": "10-01-2024"
+  },
+  {
+    "ID": 3,
+    "Amount": 50,
+    "Brand": "XYZ Corp",
+    "Category": "Grocery",
+    "Date": "05-23-2023"
+  }
+];
+
 let modalBtn = document.getElementById("createTrans")
 let modal = document.querySelector(".Modal")
 let closeBtn = document.querySelector(".close-btn")
 let subBtn=document.getElementById("save-btn")
 const tableBody = document.querySelector('tbody');
+
+Data.forEach(item => {
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${item.ID}</td>
+    <td>${item.Amount}</td>
+    <td>${item.Brand}</td>
+    <td>${item.Category}</td>
+    <td>${item.Date}</td>
+  `;
+  tableBody.appendChild(row);
+});
+
 modalBtn.onclick = function(){
   modal.style.display = "block"
 }
