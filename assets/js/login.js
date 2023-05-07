3b0883f7a548e4ff91ab13771eb5dc303d98e61d
Abhishek S <abhisheks3002@gmail.com>
2023-05-07 17:31:50 +0530

    name changed

--- assets/js/login.js
+++ assets/js/login.js
@@ -82,6 +82,7 @@ submitButton.addEventListener("click", function() {
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
+      sessionStorage.setItem('isLoggedIn', true);
       console.log("Success! Welcome back!");
       window.alert("Success! Welcome back!");
       window.location.href = "../pages/new.html";
